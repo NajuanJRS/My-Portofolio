e4cdace6903eea9710dabce3bb1a40281dfccba1
NajuanJRS <najuan600@gmail.com>
2025-11-05 14:35:18 +0800

    perbaikan

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -13,7 +13,7 @@ export default function About() {
       <h3>Riwayat Pendidikan</h3>
       <ul>
         <li><b>2023 - Sekarang:</b> Politeknik Negeri Banjarmasin – Teknik Informatika</li>
-        <li><b>2020 - 2023:</b> SMA Negeri 10 Banjarmasin - MIPA</li>
+        <li><b>2020 - 2023:</b> SMA Negeri 10 Banjarmasin</li>
         <li><b>2017 - 2020:</b>Mts Negeri 1 Banjarmasin</li>
       </ul>
     </section>
